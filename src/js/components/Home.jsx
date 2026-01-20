c0e9640c0af1b1497a2e0735948f60547c47e9e0
gusdev <59888502+Gus2708@users.noreply.github.com>
2026-01-20 03:14:40 +0000

    Todo List Finished ✅

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,65 @@
 import React from "react";
+import Switch from "./Switch";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+  const [toDoText, setToDoText] = React.useState("");
+  const [toDoList, setToDoList] = React.useState([]);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const addListItem = () => {
+    if (toDoText.trim() !== "") {
+      setToDoList([...toDoList, toDoText.trim()]);
+      setToDoText("");
+    }
+  };
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const deleteItem = (item) => {
+    const index = toDoList.indexOf(item.target.parentElement.parentElement.textContent.trim());
+    if (index > -1) {
+      const newList = [...toDoList];
+      newList.splice(index, 1);
+      setToDoList(newList);
+    };
+  };
+
+  return (
+    <div className="text-center">
+      <div className="row">
+        <div className="col">
+          <h1 className="fs-1 mt-5">My To Do list</h1>
+        </div>
+      </div>
+      <div className="row mt-5">
+        <div className="col d-flex justify-content-center">
+          <div className="inputContainer position-relative widthResponsive">
+            <input
+              className="toDoInput p-3 w-100"
+              type="text"
+              onChange={(e) => setToDoText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  addListItem();
+                }
+              }}
+              value={toDoText}
+            />
+            <button onClick={addListItem} className="btn inputButton">
+              <i className="bi bi-arrow-right-short fs-3"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <ul className="mt-4 d-flex flex-column align-items-center p-0">
+        {toDoList.map((item, index) => (
+          <li className="widthResponsive mb-3" key={index}>{item}
+            <div className="actions d-flex flex-direction-column align-items-center">
+              <Switch />
+              <i class="bi bi-trash ms-2 fs-5 trashButton" onClick={e => deleteItem(e)}></i>
+            </div></li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default Home;
